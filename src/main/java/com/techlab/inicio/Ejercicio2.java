4ebca9fe2246c84f21f3bdb7a0821202e1b00468
matiassmiron <matiassmiron@gmail.com>
2025-09-03 21:21:29 -0300

    Identacion de codigo

--- src/main/java/com/techlab/inicio/Ejercicio2.java
+++ src/main/java/com/techlab/inicio/Ejercicio2.java
@@ -10,36 +10,35 @@ public class Ejercicio2 {
 // Pedile al usuario que ingrese su nombre y la cantidad de productos que quiere comprar.
 // Mostrá un mensaje personalizado con el monto total (asignando un precio fijo por unidad).
 public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    
-    System.out.println("Ingrese su nombre: ");
-    String nombre = scanner.nextLine();
-    System.out.println("Hola "+ nombre);
-    
-    int precioProducto = 200;
+    try (Scanner scanner = new Scanner(System.in)) {
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Hola "+ nombre);
+        
+        int precioProducto = 200;
 
-    System.out.println("Ingrese la cantidad de productos que desea comprar: ");
-    int cantidadProducto = Integer.parseInt(scanner.nextLine());
-    System.out.println("El monto total es: " + cantidadProducto * precioProducto);
+        System.out.println("Ingrese la cantidad de productos que desea comprar: ");
+        int cantidadProducto = Integer.parseInt(scanner.nextLine());
+        System.out.println("El monto total es: " + cantidadProducto * precioProducto);
 
-    if (cantidadProducto > 100) {
-        System.out.println(nombre + ": ¡Le ofrecemos un descuento especial!");
-    }else{
-        System.out.println(nombre + ": El monto total es: " + cantidadProducto * precioProducto);
+        if (cantidadProducto > 100) {
+            System.out.println(nombre + ": ¡Le ofrecemos un descuento especial!");
+        }else{
+            System.out.println(nombre + ": El monto total es: " + cantidadProducto * precioProducto);
+        }
+        System.out.println(nombre + ": Ingrese un numero desde el 1: ");
+        int numero = scanner.nextInt();
+         for(int i = 1; i <= numero; i++){
+            System.out.println(i);
+         }
+         System.out.println(nombre + ": Ingrese un numero desde el 1: ");
+         int numero2 = scanner.nextInt();
+         int aux = 1;
+         while( aux <= numero2){
+            System.out.println(aux);
+            aux++;
+         }
     }
-    System.out.println(nombre + ": Ingrese un numero desde el 1: ");
-    int numero = scanner.nextInt();
-     for(int i = 1; i <= numero; i++){
-        System.out.println(i);
-     }
-     System.out.println(nombre + ": Ingrese un numero desde el 1: ");
-     int numero2 = scanner.nextInt();
-     int aux = 1;
-     while( aux <= numero2){
-        System.out.println(aux);
-        aux++;
-     }
-
     }
 
 }
